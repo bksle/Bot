0de22bc4d64585b9d56fc847933a5a718b10188f
bksle <109824989+bksle@users.noreply.github.com>
2026-06-18 02:58:15 +0300

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,13 @@
+from .display_name import (
+    format_member_label_for_channels,
+    stylize_latin_to_math_sans_bold_italic,
+    stylized_ticket_display_name,
+)
+from .math_text import to_mathematical_sans_serif_bold_italic
+
+__all__ = (
+    "format_member_label_for_channels",
+    "stylize_latin_to_math_sans_bold_italic",
+    "stylized_ticket_display_name",
+    "to_mathematical_sans_serif_bold_italic",
+)
